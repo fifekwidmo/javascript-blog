c90590c6cda0a9bde1412fbce003569a5541adaa
Filip Cwiek <fifek.inex@gmail.com>
2020-03-11 20:22:36 +0100

    Noted issue with gitAttribute

--- js/script.js
+++ js/script.js
@@ -30,4 +30,42 @@ const titleClickHandler = function(event) {
 const links = document.querySelectorAll('.titles a');
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
+
 }
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+
+    /* [DONE] remove contents of titleList */
+    let titleList = document.querySelector(optTitleListSelector);
+    console.log(titleList);
+
+    function clearMessages() {
+        titleList.innerHTML = '';
+    }
+
+    /* for each article */
+    let articles = document.querySelectorAll(optArticleSelector);
+    for (let article of articles) {
+        /* get the article id */
+        article = document.getElementsByClassName(optArticleSelector);
+        let articleId = article.getAttribute('id');
+        console.log(articleId);
+        /* find the title element */
+        /* get the title from the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+        /* create HTML of the link */
+        const linkHTML = '<li><a href="# ' + articleId + '"><span> ' + articleTitle + '</span></a></li>';
+        console.log(linkHTML);
+
+        /* insert link into titleList */
+    }
+
+
+}
+
+generateTitleLinks();
