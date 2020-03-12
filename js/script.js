7fa6efa758611b7b52db6de6bff96d0762d76c3a
Filip Cwiek <fifek.inex@gmail.com>
2020-03-12 17:02:14 +0100

    Resolve issues with get attribute and add new lines in js code

--- js/script.js
+++ js/script.js
@@ -11,61 +11,46 @@ const titleClickHandler = function(event) {
     /* [DONE] add class 'active' to the clicked link */
     const clickedElement = this;
     clickedElement.classList.add('active');
-    console.log('clickedElement (with plus): ' + clickedElement);
     /* [DONE] remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts .active');
+    const activeArticles = document.querySelectorAll('.posts article.active');
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
     /* [DONE] get 'href' attribute from the clicked link */
     let articleSelector = clickedElement.getAttribute('href');
-    console.log(articleSelector);
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
-    console.log(targetArticle);
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
 }
-
-const links = document.querySelectorAll('.titles a');
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
-
-}
-
 const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
 
 function generateTitleLinks() {
-
     /* [DONE] remove contents of titleList */
-    let titleList = document.querySelector(optTitleListSelector);
-    console.log(titleList);
-
-    function clearMessages() {
-        titleList.innerHTML = '';
-    }
-
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
     /* for each article */
-    let articles = document.querySelectorAll(optArticleSelector);
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
     for (let article of articles) {
         /* get the article id */
-        article = document.getElementsByClassName(optArticleSelector);
-        let articleId = article.getAttribute('id');
-        console.log(articleId);
+        // article = document.getElementsByClassName(optArticleSelector);
+        const articleId = article.getAttribute('id');
         /* find the title element */
         /* get the title from the title element */
         const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-
         /* create HTML of the link */
-        const linkHTML = '<li><a href="# ' + articleId + '"><span> ' + articleTitle + '</span></a></li>';
-        console.log(linkHTML);
-
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
         /* insert link into titleList */
+        html = html + linkHTML;
     }
-
-
+    titleList.innerHTML = html;
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+    for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+    }
 }
-
 generateTitleLinks();
