dcccac1099ae935ffe305132cf80cfb8a5c96bbe
Filip Cwiek <fifek.inex@gmail.com>
2020-03-12 17:12:39 +0100

    Delete console.logs

--- js/script.js
+++ js/script.js
@@ -50,7 +50,6 @@ function generateTitleLinks() {
     }
     titleList.innerHTML = html;
     const links = document.querySelectorAll('.titles a');
-    console.log(links);
     for (let link of links) {
         link.addEventListener('click', titleClickHandler);
     }
