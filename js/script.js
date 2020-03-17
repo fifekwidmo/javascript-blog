00251f5e770af7c37cb2abe226cbf2c934155210
Filip Cwiek <fifek.inex@gmail.com>
2020-03-17 20:48:43 +0100

    Perform generateTags function

--- js/script.js
+++ js/script.js
@@ -27,7 +27,9 @@ const titleClickHandler = function(event) {
 }
 const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
+    optTitleListSelector = '.titles',
+    optArticleTagsSelector = '.post-tags .list';
+
 
 function generateTitleLinks() {
     /* [DONE] remove contents of titleList */
@@ -55,3 +57,31 @@ function generateTitleLinks() {
     }
 }
 generateTitleLinks();
+
+function generateTags() {
+    /* find all articles */
+    const articles = document.querySelectorAll(optArticleSelector)
+        /* START LOOP: for every article: */
+    for (let article of articles) {
+        /* find tags wrapper */
+        const linkWrapper = article.querySelector(optArticleTagsSelector);
+        /* make html variable with empty string */
+        let html = '';
+        /* get tags from data-tags attribute */
+        const articleTags = article.getAttribute('data-tags');
+        /* split tags into array */
+        const articleTagsArray = articleTags.split(' ');
+        /* START LOOP: for each tag */
+        for (let tag of articleTagsArray) {
+            let htmlTags = '<li><a href="#tag-' + tag + '"> ' + tag + ' </a></li>';
+            /* generate HTML of the link */
+            console.log(tag);
+            /* add generated code to HTML variable */
+            html = html + htmlTags;
+        }
+        /* END LOOP: for each tag */
+        linkWrapper.innerHTML = html;
+        /* END LOOP: for every article: */
+    }
+}
+generateTags();
